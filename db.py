5496600feac797ff3f8cc25f812d53e000c2adc3
wkpn <yegornosov@gmail.com>
2018-06-22 18:52:00 +0300

    fix

--- db.py
+++ db.py
@@ -26,7 +26,7 @@ def add_to_db(chat_id, source, target):
 def get_from_db(chat_id, **kwargs):
     conn = sqlite3.connect(f'{db_name}')
     command = ', '.join(key for key in kwargs)
-
+    print(f'Get command {command}')
     data = conn.cursor().execute(f"SELECT {command} FROM users WHERE chat_id='{chat_id}'").fetchone()
     print(f'Got {data} from db for {chat_id} with kwargs {kwargs}')
 
@@ -36,7 +36,7 @@ def get_from_db(chat_id, **kwargs):
 def update_in_db(chat_id, **kwargs):
     conn = sqlite3.connect(f'{db_name}')
     command = ', '.join(f'{key}={value}' for key, value in kwargs.items())
-
+    print(f'Update command {command}')
     conn.cursor().execute(f"UPDATE users SET {command} WHERE chat_id='{chat_id}")
     conn.commit()
     conn.close()
